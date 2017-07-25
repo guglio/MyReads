806191a3f937fd8ae0bc683a1e45e65e892f2c38
Guglielmo Turco <guglielmo.turco@gmail.com>
2017-07-25 10:46:08 +0200

    Refactored the books list show while searching

--- src/Search.js
+++ src/Search.js
@@ -1,10 +1,30 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
+import ListBooks from './ListBooks.js'
 import './App.css'
 
 class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: '',
+      searchResult: []
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({search: event.target.value});
+
+    BooksAPI.search(event.target.value,20).then((books) => {
+      this.setState({searchResult: books})
+    });
+  }
+
   render(){
+
     return(
       <div className="search-books">
         <div className="search-books-bar">
@@ -18,12 +38,12 @@ class Search extends React.Component {
               However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
               you don't find a specific author or title. Every search is limited by search terms.
             */}
-            <input type="text" placeholder="Search by title or author"/>
+            <input type="text" placeholder="Search by title or author" value={this.state.search} onChange={this.handleChange}/>
 
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid"></ol>
+          <ListBooks books={this.state.searchResult} />
         </div>
       </div>
     )
