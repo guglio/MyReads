c6821904f44792cf9793e7ca523e57cf494e3c49
Guglielmo Turco <guglielmo.turco@gmail.com>
2017-07-27 11:36:31 +0200

    Change the default value for the shelf type

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -17,12 +17,12 @@ class ListBooks extends React.Component {
               <div className="book-top">
                 <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url('+books.imageLinks.thumbnail+')' }}></div>
                 <div className="book-shelf-changer">
-                  <select>
+                  <select defaultValue={books.shelf}>
                     <option value="none" disabled>Move to...</option>
                     <option value="currentlyReading">Currently Reading</option>
                     <option value="wantToRead">Want to Read</option>
                     <option value="read">Read</option>
-                    <option value="none" default>None</option>
+                    <option value="none">None</option>
                   </select>
                 </div>
               </div>
