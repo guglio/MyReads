dabb8fb93cc70a78e9ea52fc8582e5dd8fba6a67
Guglielmo Turco <guglielmo.turco@gmail.com>
2017-07-27 14:48:38 +0200

    Refactored this component, now it renders a specific Book component

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Book from './Book.js'
 
 class ListBooks extends React.Component {
   static propTypes = {
@@ -13,23 +14,7 @@ class ListBooks extends React.Component {
       <ol className="books-grid">
         {showBooks.map((books,index) => (
           <li key={index}>
-            <div className="book">
-              <div className="book-top">
-                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url('+books.imageLinks.thumbnail+')' }}></div>
-                <div className="book-shelf-changer">
-                  <select defaultValue={books.shelf}>
-                    <option value="none" disabled>Move to...</option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
-                  </select>
-                </div>
-              </div>
-              <div className="book-title">{books.title}</div>
-              <div className="book-authors">
-              {books.authors ? books.authors.map((authors,index) => <span key={index}>{authors}<br/></span>):''}</div>
-            </div>
+            <Book book={books} />
           </li>
         ))}
       </ol>
