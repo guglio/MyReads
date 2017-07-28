5462e8b781d965966de66350dd67da0a33c49841
Guglielmo Turco <guglielmo.turco@gmail.com>
2017-07-28 14:52:05 +0200

    Hide the state change if it's the 404 book

--- src/Book.js
+++ src/Book.js
@@ -9,12 +9,15 @@ class Book extends React.Component {
 
   constructor(props){
     super(props);
-
+    this.state = {
+      shelf:this.props.book.shelf
+    }
     this.shelfChange = this.shelfChange.bind(this);
     this.updateParent = this.updateParent.bind(this);
   }
 
   shelfChange(event){
+    this.setState({shelf:event.target.value});
     BooksAPI.update({id:this.props.book.id},event.target.value).then(() =>(
       this.updateParent()
     ));
@@ -26,24 +29,23 @@ class Book extends React.Component {
   }
 
   render() {
-    let book = this.props.book,
-        shelf = 'none';
-    if(this.props.handler)
-      shelf = this.props.book.shelf ? this.props.book.shelf : 'none';
+    let book = this.props.book;
 
     return(
         <div className="book">
           <div className="book-top">
             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url('+book.imageLinks.thumbnail+')' }}></div>
-            <div className="book-shelf-changer">
-              <select value={shelf} onChange={(e) => this.shelfChange(e)}>
-                <option value="" disabled>Move to...</option>
-                <option value="currentlyReading">Currently Reading</option>
-                <option value="wantToRead">Want to Read</option>
-                <option value="read">Read</option>
-                <option value="none">None</option>
-              </select>
-            </div>
+            {this.state.shelf !== 'hide' ?
+              <div className="book-shelf-changer">
+                <select value={this.state.shelf} onChange={(e) => this.shelfChange(e)}>
+                  <option value="" disabled>Move to...</option>
+                  <option value="currentlyReading">Currently Reading</option>
+                  <option value="wantToRead">Want to Read</option>
+                  <option value="read">Read</option>
+                  <option value="none">None</option>
+                </select>
+              </div>
+             : ''}
           </div>
           <div className="book-title">{book.title}</div>
           <div className="book-authors">
